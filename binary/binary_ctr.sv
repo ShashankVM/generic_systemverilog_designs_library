703f7dcca68b02d7c1c23ab0b3f90e3163f5955c
Shashank V M <58113556+ShashankVM@users.noreply.github.com>
2021-03-11 22:02:29 +0530

    Create binary_ctr.sv

---
+++ binary/binary_ctr.sv
@@ -0,0 +1,11 @@
+module binary_ctr
+  #(parameter WIDTH=4)
+  (input logic clk, 
+             reset, 
+   output logic [WIDTH-1:0] q);    
+  
+  always_ff @ (posedge clk) 
+    if (reset == 1) q <= 'b0;
+    else            q <= q + 1; 
+  
+endmodule
